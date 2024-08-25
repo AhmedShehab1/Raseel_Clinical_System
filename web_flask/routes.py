0d67398ba8829b6649d5a3f2aa4c3a8867d9079d
AhmedShehab1 <legendbtw2003@gmail.com>
2024-08-25 14:27:57 +0300

    .

--- web_flask/routes.py
+++ web_flask/routes.py
@@ -63,3 +63,7 @@ def register():
                            title='Register - Raseel',
                            form=form
                                     )
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About - Raseel")
