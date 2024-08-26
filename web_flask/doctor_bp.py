16278c4be89c421f52ba8f46820f6491d8f754f8
AhmedShehab1 <legendbtw2003@gmail.com>
2024-08-26 19:43:54 +0300

    Linting Issues Fixed

--- web_flask/doctor_bp.py
+++ web_flask/doctor_bp.py
@@ -2,10 +2,14 @@ from flask import Blueprint, render_template
 
 doctor_bp = Blueprint('doctor_bp', __name__, url_prefix='/doctor')
 
+
 @doctor_bp.route('/appointments/current')
 def current_appointments():
-    return render_template('doctor/current.html', title='Current Appointments')
+    return render_template('doctor/current.html',
+                           title='Current Appointments')
+
 
 @doctor_bp.route('/appointments/upcoming')
 def upcoming_appointments():
-    return render_template('doctor/upcoming.html', title='Upcoming Appointments')
+    return render_template('doctor/upcoming.html',
+                           title='Upcoming Appointments')
