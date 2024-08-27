4baa622e8e8872581669e99b9ebd42f825361997
AhmedShehab1 <legendbtw2003@gmail.com>
2024-08-27 10:36:05 +0300

    Add password reset functionality
    - Added JWT-based password reset token generation and verification in [`Patient`]
    - Updated [`requirements.txt`]
    - Initialized [`flask_mail`]
    - Created forms for password reset request and reset in [`web_flask/forms.py`]
    - Added routes for password reset request and reset in [`web_flask/routes.py`]
    - Updated [`login.html`]
    - Enhanced [`about.html`]
    - Added new CSS styles for mission section in [`styles.css`]

---
+++ web_flask/email.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from web_flask import app, mail
+from flask_mail import Message
+from threading import Thread
+
+
+def send_email_async(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_email_async, args=(app, msg)).start()
+
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('[Raseel] Reset Your Password',
+               sender=app.config['MAIL_DEFAULT_SENDER'],
+               recipients=[user.email],
+               text_body=render_template('email/reset_password.txt',
+                                         user=user, token=token),
+               html_body=render_template('email/reset_password.html',
+                                         user=user, token=token))
+    
