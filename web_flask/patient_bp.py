b8b446d0b9abfe0a140805356446942499886bd6
MazenAtlam <mazenatef5510@gmail.com>
2024-09-01 13:42:22 +0300

    eat#3: Patient Pages - Create a base_patient.html and patient_navigation.html files, Add Home, About and Book Appiontment Pages and Add these pages to the patient blueprint feat#4: Receptionist pages - Create a base_receptionist.html and receptionist_navigation.html files, Add Home, About and Medical Departments Pages and Add these pages to the receptionist blueprint

--- web_flask/patient_bp.py
+++ web_flask/patient_bp.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template
+import models as m
+from flask import Blueprint, render_template, request, flash
+from web_flask import db
+from web_flask.main.forms import (
+    VisitorForm
+)
+import sqlalchemy as sa
 
 
 patient_bp = Blueprint("patient_bp", __name__, url_prefix="/patient")
@@ -12,7 +18,7 @@ def medical_departments():
         str: Render the patient template for veiwing the medical departments
     """
 
-    return render_template("patient/medical_departments.html", title="Medical Departments")
+    return render_template("patient/medical_departments.html", title="Medical Departments - Raseel")
 
 @patient_bp.route("/book-appointment")
 def patient_book_appointment():
@@ -24,3 +30,23 @@ def patient_book_appointment():
     """
 
     return render_template("patient/book_appointment.html", title="Book Appointment")
+
+@patient_bp.route("/about")
+def about():
+    """
+    The about us page of the raseel center for the patient
+
+    Returns:
+        str: Render the patient template for view the about us page
+    """
+
+    doctors = db.session.scalars(sa.select(m.Doctor))
+    return render_template("patient/about.html", title="About - Raseel", doctors=doctors)
+
+@patient_bp.route("/", methods=["GET", "POST"])
+def index():
+    form = VisitorForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            flash("Your message has been sent!", "success")
+    return render_template("patient/index.html", title="Home - Raseel", form=form)
--- web_flask/receptionist_bp.py
+++ web_flask/receptionist_bp.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+import models as m
+from flask import Blueprint, render_template, request, flash
+from web_flask import db
+from web_flask.main.forms import (
+    VisitorForm
+)
+import sqlalchemy as sa
 
 
 receptionist_bp = Blueprint("receptionist_bp", __name__, url_prefix="/receptionist")
+
+@receptionist_bp.route("/medical-departments")
+def medical_departments():
+    """
+    A page to show the medical departments in the raseel medical center
+
+    Returns:
+        str: Render the receptionist template for veiwing the medical departments
+    """
+
+    return render_template("receptionist/medical_departments.html", title="Medical Departments - Raseel")
 
 @receptionist_bp.route("/book-appointment")
 def book_appointment():
@@ -9,7 +26,27 @@ def book_appointment():
     Book an appointment for a patient
 
     Returns:
-        str: Render the reciptionist template for booking an appointiment
+        str: Render the receptionist template for booking an appointiment
     """
 
-    return render_template("receptionist/book_appointment.html", title="Book Appointment")
+    return render_template("receptionist/book_appointment.html", title="Book Appointment - Raseel")
+
+@receptionist_bp.route("/about")
+def about():
+    """
+    The about us page of the raseel center for the receptionist
+
+    Returns:
+        str: Render the receptionist template for view the about us page
+    """
+
+    doctors = db.session.scalars(sa.select(m.Doctor))
+    return render_template("receptionist/about.html", title="About - Raseel", doctors=doctors)
+
+@receptionist_bp.route("/", methods=["GET", "POST"])
+def index():
+    form = VisitorForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            flash("Your message has been sent!", "success")
+    return render_template("receptionist/index.html", title="Home - Raseel", form=form)
