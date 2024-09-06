71c9bccbfe08e2385adbabff1dcd23a8fac2745e
MazenAtlam <mazenatef5510@gmail.com>
2024-09-06 05:22:58 +0300

    feat: receptionist book appointment page - Add a departments api, and make the department and doctor data changes dynamically

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,5 +4,6 @@ from flask import Blueprint
 bp = Blueprint('api', __name__, url_prefix='/api/v1')
 
 from api.v1.views.appointments import *
+from api.v1.views.departments import *
 from api.v1.errors import *
 from api.v1.views.patients import *
