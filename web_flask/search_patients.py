0f42e739ffb0a894f851e567fd8b4426e995a826
MazenAtlam <mazenatef5510@gmail.com>
2024-09-11 20:29:03 +0300

    feat: book appointment: - fix search query problem - change bg for all logged_in pages - update the database

--- web_flask/search_patients.py
+++ web_flask/search_patients.py
@@ -5,7 +5,8 @@ def search_patients():
     session['previous_endpoint'] = request.endpoint
     if g.search_form.validate():
         page = request.args.get("page", 1, type=int)
-        search_results, _ = m.Patient.search(g.search_form.q.data, page,
+        print(g.search_form.q.data, page, current_app.config.get('SEARCH_RESULTS_PER_PAGE', 10))
+        search_results = m.Patient.search(g.search_form.q.data, page,
                                                 current_app.config.get('SEARCH_RESULTS_PER_PAGE', 10))
         return search_results
     return None
