8c14fb129d4dba93972512dda26f2a2365b6efe3
AhmedShehab1 <legendbtw2003@gmail.com>
2024-09-12 15:15:29 +0300

    Added Icons instead of text

--- models/receptionist.py
+++ models/receptionist.py
@@ -3,13 +3,13 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 
 
-class Receptionist(m.StaffMember):
-    """
-    Receptionist model class
-    Args:
-        BaseModel (): Base model class
-        PasswordMixin (): Password mixin class
-    """
+# class Receptionist(m.StaffMember):
+#     """
+#     Receptionist model class
+#     Args:
+#         BaseModel (): Base model class
+#         PasswordMixin (): Password mixin class
+#     """
 
-    __tablename__ = "receptionists"
+#     __tablename__ = "receptionists"
 
